9aa75ceec4a20a6498363af8c478b2312563fd80
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-20 16:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function enter()
+{
+	let status=document.getElementById("status");
+	let heading=documet.createElement("h1");
+	heading.textContent=status.textContent;
+	status.parentNode.replaceChild(heading, status); 
+
+}
